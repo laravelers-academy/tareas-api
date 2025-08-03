30d68026b9918cb91abb45ba21051d065844b189
Homero Raul <h.raulvc@gmail.com>
2025-08-03 09:12:07 -0600

    Refactor TaskController to use TaskRequest for validation and enhance JSON responses with messages

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Task;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,38 +14,57 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return response()->json([
+            'message' => 'Listado de tareas',
+            'data' => $tasks
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+        return response()->json([
+            'message' => 'Tarea creada correctamente',
+            'data' => $task
+        ], 201);
     }
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return response()->json([
+            'message' => 'Detalle de la tarea',
+            'data' => $task
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+        return response()->json([
+            'message' => 'Tarea actualizada correctamente',
+            'data' => $task
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->json([
+            'message' => 'Tarea eliminada correctamente'
+        ]);
     }
 }
